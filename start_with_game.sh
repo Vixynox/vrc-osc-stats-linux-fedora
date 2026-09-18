9f504ed3a8ed48771e58ad1268bceac1522417d4
Vixynox <197238925+Vixynox@users.noreply.github.com>
2026-09-19 00:56:34 +0200

    Fix script execution for OSC in start_with_game.sh

--- start_with_game.sh
+++ start_with_game.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Uruchamia skrypt OSC w tle (korzystając z Twojego wirtualnego środowiska)
-/home/$USER/vrc-osc-stats/venv/bin/python /home/$USER/vrc-osc-stats/skrypt_osc.py &
+/home/$USER/vrc-osc-stats/venv/bin/python /home/$USER/vrc-osc-stats/scrypt_osc.py &
 OSC_PID=$!
 
-# Uruchamia VRChat (Steam podstawi tu komendę startową)
 "$@"
 
-# Po wyłączeniu gry, zabija skrypt OSC działający w tle
 kill $OSC_PID
